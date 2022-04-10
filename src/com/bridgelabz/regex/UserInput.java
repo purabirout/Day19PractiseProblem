1e9414302688308b15a4ef1e47131914ce939672
Purabi Rout <96911661+purabirout@users.noreply.github.com>
2022-04-11 00:36:14 +0530

    Add files via upload

---
+++ src/com/bridgelabz/regex/UserInput.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+
+public class UserInput {
+	public static final Scanner scn = new Scanner(System.in);
+	
+	public static final String StringInput() {
+		return scn.next();
+	}
+}
